77412fc748135c0d1da37e124943e986f8d91fe5
Kristian Glass <git@doismellburning.co.uk>
2014-10-21 10:55:02 +0200

    "Import" of Django tools from laterpay-client-python

---
+++ README.rst
@@ -0,0 +1,36 @@
+laterpay-client-python
+======================
+
+.. image:: https://badge.fury.io/py/laterpay-client.png
+    :target: http://badge.fury.io/py/laterpay-client
+
+.. image:: https://travis-ci.org/laterpay/laterpay-client-python.png?branch=develop
+    :target: https://travis-ci.org/laterpay/laterpay-client-python
+
+.. image:: https://coveralls.io/repos/laterpay/laterpay-client-python/badge.png?branch=develop
+    :target: https://coveralls.io/r/laterpay/laterpay-client-python
+
+.. image:: https://pypip.in/d/laterpay-client/badge.png
+    :target: https://crate.io/packages/laterpay-client?version=latest
+
+
+`LaterPay <http://www.laterpay.net/>`__ Python client
+
+Installation
+------------
+
+::
+
+    $ pip install laterpay-client
+
+Usage
+-----
+
+See https://www.laterpay.net/developers/docs
+
+Development
+-----------
+
+See https://github.com/laterpay/laterpay-client-python
+
+`Tested by Travis <https://travis-ci.org/laterpay/laterpay-client-python>`__
---
+++ setup.py
@@ -0,0 +1,43 @@
+# -*- coding: UTF-8 -*-
+from setuptools import find_packages, setup
+
+import codecs
+import os
+
+_version = "1.0.0"
+_packages = find_packages('.', exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
+
+if os.path.exists('README.rst'):
+    _long_description = codecs.open('README.rst', 'r', 'utf-8').read()
+else:
+    _long_description = ""
+
+setup(
+    name='django-laterpay',
+    version=_version,
+
+    description="LaterPay Django tools",
+    long_description=_long_description,
+    author="LaterPay GmbH",
+    author_email="support@laterpay.net",
+    url="https://github.com/laterpay/laterpay-client-python",
+    license='MIT',
+    keywords="LaterPay API client Django",
+
+    test_suite="tests",
+
+    packages=_packages,
+    package_data={'djlaterpay': ['templates/laterpay/inclusion/*']},
+
+    classifiers=(
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ),
+)
