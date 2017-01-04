b7b9869a25753bad19f433843589068440b1d42c
Kristian Glass <git@doismellburning.co.uk>
2017-01-04 10:14:42 +0000

    Added extra blank lines after function definition for flake8
    E305 expected 2 blank lines after class or function definition, found 1

--- djlaterpay/templatetags/laterpay.py
+++ djlaterpay/templatetags/laterpay.py
@@ -19,6 +19,7 @@ def render_laterpay_header(context):
 
     return {'web_root': web_root}
 
+
 register.inclusion_tag('laterpay/inclusion/render_header.html', takes_context=True)(render_laterpay_header)
 
 
@@ -28,6 +29,7 @@ def render_laterpay_footer(context, identify_callback=None):
     laterpay = context['request'].laterpay
     return {'identify_url': laterpay.get_identify_url(identify_callback)}
 
+
 register.inclusion_tag('laterpay/inclusion/render_footer.html', takes_context=True)(render_laterpay_footer)
 
 
@@ -35,11 +37,13 @@ def laterpay_subscribe(context, product_key=None, subscription_key=None):
     laterpay = context['request'].laterpay
     return laterpay.get_subscription_url(product_key, subscription_key)
 
+
 register.simple_tag(takes_context=True)(laterpay_subscribe)
 
 
 def laterpay_buy(context, item_definition, product_key=None):
     laterpay = context['request'].laterpay
     return laterpay.get_buy_url(item_definition, product_key)
+
 
 register.simple_tag(takes_context=True)(laterpay_buy)
